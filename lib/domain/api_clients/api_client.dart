aee1c55bf18acc2c71cf4268324947f88fa993f6
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-04 19:41:09 +0300

    getPost

--- lib/domain/api_clients/api_client.dart
+++ lib/domain/api_clients/api_client.dart
@@ -1,24 +1,35 @@
 import 'dart:convert';
-import 'dart:developer';
 import 'dart:io';
+
+import 'package:work_with_http/domain/entity/post.dart';
 
 class ApiClient {
   final client = HttpClient();
 
-  void getPost() async {
-    final url = Uri.parse("https://jsonplaceholder.typicode.com/posts");
-    // final url = Uri(
+  Future<List<Post>> getPost() async {
+    final json = await get("https://jsonplaceholder.typicode.com/posts")
+        as List<dynamic>;
+
+    final posts =
+        json.map((e) => Post.fromJson(e as Map<String, dynamic>)).toList();
+    return posts;
+  }
+
+  Future<dynamic> get(String str) async {
+    final url = Uri.parse(str);
+    final request = await client.getUrl(url);
+    final response = await request.close();
+
+    final jsonStrings = await response.transform(utf8.decoder).toList();
+    final jsonString = jsonStrings.join();
+    final json = jsonDecode(jsonString);
+
+    return json;
+  }
+}
+
+// final url = Uri(
     //   scheme: 'https',
     //   host: "jsonplaceholder.typicode.com",
     //   path: "posts",
     // );
-    final request = await client.getUrl(url);
-    final response = await request.close();
-    response.transform(utf8.decoder).listen(
-          (event) {
-            log(event);
-            log('1');
-          },
-        );
-  }
-}

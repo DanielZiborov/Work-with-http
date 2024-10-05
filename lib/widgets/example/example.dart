abf6deeb840e6fa7a213b3565af5e9296beb2ec1
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-05 19:23:15 +0300

    example remake1

--- lib/widgets/example/example.dart
+++ lib/widgets/example/example.dart
@@ -1,20 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:work_with_http/domain/api_clients/api_client.dart';
+import 'package:work_with_http/widgets/example/example_model.dart';
 
-class Example extends StatelessWidget {
+class Example extends StatefulWidget {
   const Example({super.key});
+  @override
+  State<Example> createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  final model = ExampleWidgetModel();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ExampleModelProvider(
+          model: model,
+          child: const Column(
+            children: [
+              const ReloadButton(),
+              const CreateButton(),
+              Expanded(
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  // child: _PostsWidget(),
+                  child:Placeholder(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ReloadButton extends StatelessWidget {
+  const ReloadButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    ApiClient().getPost();
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Example"),
-        centerTitle: true,
-      ),
-      body: SafeArea(
-        child: Container(),
-      ),
-    );
+    return const Placeholder();
+  }
+}
+
+class CreateButton extends StatelessWidget {
+  const CreateButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
